b7c9925f303e219dc876dcd06aeef004ba3e0174
KEVIN_NOGUEIRA <kevinnog@outlook.com.br>
2020-10-21 22:58:11 -0300

    Accessing and rendering data from the database

--- src/trying_django/settings.py
+++ src/trying_django/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # own
     'products',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'trying_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
